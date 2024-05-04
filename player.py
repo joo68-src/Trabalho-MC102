3086ef9f64904791976d011efcc10fb3b474f249
joão lucena <84159438+joo68-src@users.noreply.github.com>
2024-05-04 17:19:10 -0300

    Update player.py
    adicionei parte da análise de cores certas

--- player.py
+++ player.py
@@ -15,8 +15,13 @@ from random import sample
 # Cores disponíveis para o palpite
 colors = [RED, GREEN, BLUE, YELLOW, ORANGE, BLACK, WHITE]
 
+cores_certas = [False, False, False, False, False, False, False]
+
 
 def player(guess_hist, res_hist):
+    global colors
+    global cores_certas
+
     """
     As cores disponíveis são: RED, GREEN, BLUE, YELLOW, ORANGE, BLACK, WHITE.
 
@@ -34,7 +39,14 @@ def player(guess_hist, res_hist):
         chute = colors [len(guess_hist) : len(guess_hist) + 3]
     elif len (guess_hist) == 4:
         chute = colors [ORANGE, BLACK, WHITE, RED]
-    else: 
-    	
+    else:
+        for i in range(len(res_hist) - 1):
+            if res_hist[i + 1] > res_hist[i]:
+                cores_certas[colors.index(guess_hist[i + 1][3])] == True
+            elif res_hist[i + 1] < res_hist[i]:
+                cores_certas[colors.index(guess_hist[i][0])] == True
+            else: 
+                cores_certas[colors.index(guess_hist[i + 1][3])] == colors.index(guess_hist[i][0])
+
     return chute
     # return sample(colors, 4)  Exemplo: retorna um palpite aleatório
