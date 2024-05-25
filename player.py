d84858200770a3b7b6cb2ae54d8af254f4b856a2
joão lucena <84159438+joo68-src@users.noreply.github.com>
2024-05-25 07:30:45 -0300

    Update player.py

--- player.py
+++ player.py
@@ -6,8 +6,10 @@ from random import sample
 # Cores disponíveis para o palpite
 colors = [RED, GREEN, BLUE, YELLOW, ORANGE, BLACK, WHITE]
 
-# Dicionário com as relações das cores. As com chave "True" são cores que estão na senha, e as com chave "False" não estão na senha
-relacoes = {True : [], False : [], "dependencias" : []}
+# Dicionário com as relações das cores. As com chave "verdadeiro" são cores que estão na senha, e as com chave "falso" não estão na senha
+relacoes = {"verdadeiro" : [], "falso" : [], "dependencias" : []}
+
+respostas_possiveis = []
 
 def player(guess_hist, res_hist):
     
@@ -17,12 +19,11 @@ def player(guess_hist, res_hist):
         chute = [ORANGE, BLACK, WHITE, RED]
     elif res_hist[len(res_hist) - 1][0] == 4:
         cores_codigo = guess_hist[len(guess_hist) - 1]
-        chute = sample(cores_codigo)
-    else:    
+        chute = sample(cores_codigo, 4)
+    else:
         chute = analise(guess_hist, res_hist)
 
     return chute 
-
 
 def analise (hist_chutes, hist_result):
 
@@ -34,11 +35,11 @@ def analise (hist_chutes, hist_result):
         Se permanecer constante: as cores que entrou e que saiu são interdependentes. Se uma estiver na senha a outra também está, e vice-versa.
         '''
         if hist_result[i + 1][0] > hist_result[i][0]:
-            relacoes[True].append(colors.index(hist_chutes[i + 1][3]))
-            relacoes[False].append(colors.index(hist_chutes[i][0]))
+            relacoes["verdadeiro"].append(hist_chutes[i + 1][3])
+            relacoes["falso"].append(hist_chutes[i][0])
         elif hist_result[i + 1][0] < hist_result[i][0]:
-            relacoes[True].append(colors.index(hist_chutes[i][0]))
-            relacoes[False].append(colors.index(hist_chutes[i+1][3]))
+            relacoes["verdadeiro"].append(hist_chutes[i][0])
+            relacoes["falso"].append(hist_chutes[i+1][3])
         else:
             repeticao = False
 
@@ -47,21 +48,25 @@ def analise (hist_chutes, hist_result):
                     if relacoes["dependencias"][l][k] == hist_chutes[i + 1][3]:
                         relacoes["dependencias"][l].append(hist_chutes[i][0])
                         repeticao = True
-                    
-            for l in range(len(relacoes[True])):
-                if relacoes[True][l] == hist_chutes[i][0]:
-                    relacoes[True].append(hist_chutes[i + 1][3])
+                    if relacoes["dependencias"][l][k] == hist_chutes[i][0]:
+                        relacoes["dependencias"][l].append(hist_chutes[i + 1][3])
+                        repeticao = True
+
+            for l in range(len(relacoes["verdadeiro"])):
+                if relacoes["verdadeiro"][l] == hist_chutes[i][0]:
+                    relacoes["verdadeiro"].append(hist_chutes[i + 1][3])
                     repeticao = True
-                elif relacoes[True][l] == hist_chutes[i + 1][3]:
-                    relacoes[True].append(hist_chutes[i][0])
+                elif relacoes["verdadeiro"][l] == hist_chutes[i + 1][3]:
+                    relacoes["verdadeiro"].append(hist_chutes[i][0])
                     repeticao = True
 
             if not repeticao:
-                relacoes["dependencias"].append([colors.index(hist_chutes[i][0]), colors.index(hist_chutes[i+1][3])])
+                relacoes["dependencias"].append([hist_chutes[i][0], hist_chutes[i+1][3]])
 
     for i in range(len(relacoes["dependencias"])):
-        if len(relacoes[True]) + len(relacoes["dependencias"][i]) == 4:
-            relacoes[True].extend(relacoes["dependencias"][i])
-            relacoes["dependencias"].remove(relacoes["dependencias"][i])
+        if len(relacoes["verdadeiro"]) + len(relacoes["dependencias"][i]) == 4:
+            respostas_possiveis.append(relacoes["verdadeiro"] + relacoes["dependencias"][i])
 
-            return list(relacoes[True]) 
+    resposta = respostas_possiveis[0]
+    respostas_possiveis.remove(respostas_possiveis[0])
+    return resposta
